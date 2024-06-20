9deadc748f7f7845facaa8a556e375441bc76472
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-06-20 09:33:30 -0300

    adicionado a opção de descrição

--- src/components/mainTodoList.vue
+++ src/components/mainTodoList.vue
@@ -3,12 +3,12 @@ import { ref } from 'vue'
 
 const dataList = ref([])
 
-const newTitle = ref({ title: '' })
+const newTitle = ref({})
 
 function submitForm() {
     if (newTitle.value.title){
         dataList.value.push({ ...newTitle.value })
-        newTitle.value.title = ''
+        newTitle.value = {}
     }else{
         alert('O campo não pode estar vazio')
     }
@@ -23,11 +23,17 @@ function deleteList(index){
         <h1>Todo List</h1>
         <form @submit.prevent="submitForm">
             <input v-model="newTitle.title" placeholder="Digite o titulo">
+            <textarea v-model="newTitle.description" placeholder="Digite a descrição" rows="5" cols="10"></textarea>
             <button type="submit">Criar Tarefa</button>
         </form>
         <ul>
             <li v-for="list, index in dataList" :key="index">
                 {{ list.title }}
+                <ul>
+                    <li>
+                        {{ list.description }}
+                    </li>
+                </ul>
                 <button @click="deleteList(index)">X</button>
             </li>
         </ul>
@@ -36,6 +42,7 @@ function deleteList(index){
 
 <style scoped>
 input,
+textarea,
 button {
     background-color: transparent;
 }
