f7e29508a6a6a34a0759fd8fe5cf0378ebfad97f
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-06-21 14:39:35 -0300

    Estrutura base do vuex

---
+++ src/stone/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'vuex';
+
+//estrutura base
+const store = createStore({
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  getters: {
+
+  },
+});
+
+export default store;
