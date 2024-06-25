00d708678e0b7d7664db19e278a49b2706296c37
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-06-25 08:52:05 -0300

    aplicação do vuex

--- src/stone/index.js
+++ src/stone/index.js
@@ -3,16 +3,35 @@ import { createStore } from 'vuex';
 //estrutura base
 const store = createStore({
   state: {
-
+    dataList: []
   },
   mutations: {
-
+    addTask(state, item){
+      state.dataList.push({
+        ...item,
+        complite: false
+      })
+    },
+    completeTask(state, index){
+      state.dataList[index].complete = !state.dataList[index].complete
+    },
+    deleteTask(state, index){
+      state.dataList.splice(index, 1)
+    }
   },
   actions: {
-
+    addTask({commit}, item){
+      commit('addTask', item)
+    },
+    completeTask({commit}, item){
+      commit('completeTask', item)
+    },
+    deleteTask({commit}, item){
+      commit('deleteTask', item)
+    }
   },
   getters: {
-
+    dataList: (state) => state.dataList
   },
 });
 
