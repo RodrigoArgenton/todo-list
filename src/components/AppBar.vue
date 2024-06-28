7f56e5b9c6b5dfcecaa421a062b78b25f5c19e39
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-06-28 09:45:38 -0300

    Nav Bar básico

---
+++ src/components/AppBar.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+const drawer = ref(false)
+const items = [
+    {
+        title: 'Home',
+        value: 'Home',
+    },
+    {
+        title: 'Adicionar Task',
+        value: 'bar',
+    },
+    {
+        title: 'Listar Tasks',
+        value: 'fizz',
+    },
+]
+</script>
+<template>
+    <v-app-bar prominent>
+      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Todo List</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" temporary>
+      <v-list :items="items"></v-list>
+    </v-navigation-drawer>
+</template>
