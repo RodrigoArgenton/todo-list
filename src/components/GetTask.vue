1b178ab071848c1111f9be1ffcd9b653f40daf76
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-07-08 14:22:15 -0300

    função de excluir task

--- src/components/GetTask.vue
+++ src/components/GetTask.vue
@@ -4,16 +4,37 @@ import { useStore } from 'vuex'
 
 const store = useStore()
 const dataList = computed(() => store.getters.dataList)
+
+function deleteTask(index){
+    store.dispatch('deleteTask', index)
+}
 </script>
 
 <template>
     <v-card width="900">
-        <v-list-item v-for="item in dataList" :key="item.id">
+        <v-list-item v-for="item, index in dataList" :key="index">
             <v-list-item-title>{{ item.title }}</v-list-item-title>
             <v-list-item-subtitle>{{ item.description }}</v-list-item-subtitle>
             <div class="d-flex justify-end ">
-                <v-btn prepend-icon="mdi-trash-can-outline" variant="plain" density="comfortable" size="small" class="ma-2">Excluir</v-btn>
-                <v-btn prepend-icon="mdi-check" variant="tonal" density="comfortable" size="small" class="ma-2">Concluir</v-btn>
+                <v-btn 
+                    prepend-icon="mdi-trash-can-outline" 
+                    variant="plain" 
+                    density="comfortable" 
+                    size="small" 
+                    class="ma-2"
+                    @click="deleteTask(index)"
+                >
+                    Excluir
+                </v-btn>
+                <v-btn 
+                    prepend-icon="mdi-check" 
+                    variant="tonal" 
+                    density="comfortable" 
+                    size="small" 
+                    class="ma-2"
+                    >
+                    Concluir
+                </v-btn>
             </div>
             <v-divider></v-divider>
         </v-list-item>
--- src/store/index.js
+++ src/store/index.js
@@ -5,21 +5,27 @@ const store = createStore({
   state: {
     isDialogOpen: false,
     dataList: [
-      { id: 1, description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias illo voluptates obcaecati consequatur ea sequi assumenda at, repellendus culpa repudiandae odio corporis, sed cumque magni cupiditate. Eum eveniet porro voluptatum.", title: "teste 1" },
-      { id: 2, description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias illo voluptates obcaecati consequatur ea sequi assumenda at, repellendus culpa repudiandae odio corporis, sed cumque magni cupiditate. Eum eveniet porro voluptatum.", title: "teste 2" },
-      { id: 3, description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias illo voluptates obcaecati consequatur ea sequi assumenda at, repellendus culpa repudiandae odio corporis, sed cumque magni cupiditate. Eum eveniet porro voluptatum.", title: "teste 3" },
-      { id: 4, description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias illo voluptates obcaecati consequatur ea sequi assumenda at, repellendus culpa repudiandae odio corporis, sed cumque magni cupiditate. Eum eveniet porro voluptatum.", title: "teste 4" },
-      { id: 5, description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias illo voluptates obcaecati consequatur ea sequi assumenda at, repellendus culpa repudiandae odio corporis, sed cumque magni cupiditate. Eum eveniet porro voluptatum.", title: "teste 5" },
+      {description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias illo voluptates obcaecati consequatur ea sequi assumenda at, repellendus culpa repudiandae odio corporis, sed cumque magni cupiditate. Eum eveniet porro voluptatum.", title: "teste 1" },
+      {description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias illo voluptates obcaecati consequatur ea sequi assumenda at, repellendus culpa repudiandae odio corporis, sed cumque magni cupiditate. Eum eveniet porro voluptatum.", title: "teste 2" },
+      {description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias illo voluptates obcaecati consequatur ea sequi assumenda at, repellendus culpa repudiandae odio corporis, sed cumque magni cupiditate. Eum eveniet porro voluptatum.", title: "teste 3" },
+      {description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias illo voluptates obcaecati consequatur ea sequi assumenda at, repellendus culpa repudiandae odio corporis, sed cumque magni cupiditate. Eum eveniet porro voluptatum.", title: "teste 4" },
+      {description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias illo voluptates obcaecati consequatur ea sequi assumenda at, repellendus culpa repudiandae odio corporis, sed cumque magni cupiditate. Eum eveniet porro voluptatum.", title: "teste 5" },
   ]
   },
   mutations: {
     popup(state){
       state.isDialogOpen = !state.isDialogOpen
+    },
+    deleteTask(state, index){
+      state.dataList.splice(index, 1)
     }
   },
   actions: {
     popup({commit}){
       commit('popup')
+    },
+    deleteTask({commit}, index){
+      commit('deleteTask', index)
     }
   },
   getters: {
