027f59c61702f3b11e881f90d1d52e8e36c49bdc
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-07-09 14:19:51 -0300

    card - tarefa em aberto

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,36 @@
 <script setup>
-
+const teste = 80
 </script>
 <template>
-    <h1>Home</h1>
+    <v-card
+        prepend-icon="mdi-clock-alert-outline"
+        width="400"
+    >
+        <template v-slot:title>
+            <v-card-item>
+                <v-card-title>Tarefas em aberto</v-card-title>
+            </v-card-item>
+        </template>
+        <v-card-text class="text-h2">
+            <v-row>
+                <v-col cols="6" class="text-center">
+                    10
+                </v-col>
+                <v-col cols="6">
+                    <div class="text-center">
+                        <v-progress-circular :model-value="teste" :rotate="360" :size="100" :width="15" color="teal" class="text-h6">
+                            <template v-slot:default> {{ teste }}% </template>
+                        </v-progress-circular>
+                    </div>
+                </v-col>
+            </v-row>
+        </v-card-text>
+        <v-divider></v-divider>
+
+        <v-card-actions>
+            <v-btn
+                to="/task"
+            >Ver todas as tarefas</v-btn>
+        </v-card-actions>
+    </v-card>
 </template>
