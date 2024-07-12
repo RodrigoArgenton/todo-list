6d47d5f7e2ee372fb507a3400cbb1d6f2d33e0dd
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-07-12 10:02:25 -0300

    tela - timeline task

---
+++ src/components/home/TimelineTask.vue
@@ -0,0 +1,100 @@
+<script setup>
+
+</script>
+<template>
+    <v-timeline class="w-75">
+        <v-timeline-item
+        dot-color="purple-lighten-3"
+        fill-dot
+        >
+            <v-card>
+                <v-card-title class="bg-purple-lighten-3">
+                <v-icon
+                    class="me-4"
+                    icon="mdi-alert-outline"
+                    size="large"
+                    color="red"
+                ></v-icon>
+                <h2 class="font-weight-light">
+                    Title 1
+                </h2>
+                </v-card-title>
+                <v-card-text>
+                Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit.
+                </v-card-text>
+            </v-card>
+        </v-timeline-item>
+
+        <v-timeline-item
+            dot-color="deep-purple-lighten-2"
+            size="x-small"
+            fill-dot
+        >
+            <v-card>
+                <v-card-title class="bg-deep-purple-lighten-2 justify-end">
+                <v-icon
+                    class="me-4"
+                    icon="mdi-alert-outline"
+                    size="large"
+                    color="red"
+                ></v-icon>
+                <h2 class="me-4 font-weight-light">
+                    Title 2
+                </h2>
+                </v-card-title>
+                <v-card-text>
+                Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit.
+                </v-card-text>
+            </v-card>
+        </v-timeline-item>
+
+        <v-timeline-item
+        dot-color="cyan-lighten-1"
+        fill-dot
+        >
+            <v-card>
+                <v-card-title class="bg-cyan-lighten-1">
+                <h2 class="font-weight-light">
+                    Title 3
+                </h2>
+                </v-card-title>
+                <v-card-text>
+                Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit.
+                </v-card-text>
+            </v-card>
+        </v-timeline-item>
+
+        <v-timeline-item
+        dot-color="red-lighten-1"
+        size="x-small"
+        fill-dot
+        >
+            <v-card>
+                <v-card-title class="bg-red-lighten-1 justify-end">
+                <h2 class="me-4 font-weight-light">
+                    Title 4
+                </h2>
+                </v-card-title>
+                <v-card-text>
+                Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit.
+                </v-card-text>
+            </v-card>
+        </v-timeline-item>
+
+        <v-timeline-item
+        dot-color="green-lighten-1"
+        fill-dot
+        >
+            <v-card>
+                <v-card-title class="bg-green-lighten-1">
+                <h2 class="font-weight-light">
+                    Title 5
+                </h2>
+                </v-card-title>
+                <v-card-text>
+                Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit.
+                </v-card-text>
+            </v-card>
+        </v-timeline-item>
+    </v-timeline>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
     import OpenTaskReport from '../components/home/OpenTaskReport.vue'
     import CreatedTaskReport from '../components/home/CreatedTaskReport.vue'
     import ProductionTaskReport from '../components/home/ProductionTaskReport.vue'
+    import TimelineTask from '../components/home/TimelineTask.vue'
 </script>
 <template>
     <div class="d-flex ga-2">
@@ -14,5 +15,8 @@
         <div>
             <ProductionTaskReport/>
         </div>
+    </div>
+    <div class="d-flex justify-center pa-5">
+        <TimelineTask/>
     </div>
 </template>
