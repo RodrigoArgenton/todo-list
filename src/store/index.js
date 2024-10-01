35520c86c6a9726b299e09107867d8ecf7d8de1a
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-10-01 09:11:52 -0300

    Melhorias na lógica dos getters

--- src/store/index.js
+++ src/store/index.js
@@ -70,15 +70,20 @@ const store = createStore({
     isDialogOpen: state => state.isDialogOpen,
     dataList: state => state.dataList,
     openTask: state => state.dataList.filter(task => !task.complete).length,
-    allTask: state => state.dataList.length,
+    allTask: state => state.dataList,
+    tasksDue: (state) => {
+      const today = new Date().toISOString().split('T')[0]
+      return state.dataList.filter(task => task.dueDate.split('T')[0] < today && task.complete === false)
+    },
     tasksDueToday: (state) => {
       const today = new Date().toISOString().split('T')[0]
-      return state.dataList.filter(task => task.dueDate.split('T')[0] === today && task.complete === false).length
+      return state.dataList.filter(task => task.dueDate.split('T')[0] === today && task.complete === false)
     },
     tasksDueAll: (state) => {
       const today = new Date().toISOString().split('T')[0]
       return state.dataList.filter(task => task.dueDate.split('T')[0] < today && task.complete === false).length
-    }
+    },
+    CompletedTask: state => state.dataList.filter(task => task.complete),
   }
 })
 
